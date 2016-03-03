b8055401c73283e43fa2eac05f7e09ded3af4d32
RobertAKARobin <robertgfthomas@gmail.com>
2016-03-03 10:14:23 -0500

    Added Robin's solution

--- atm.js
+++ atm.js
@@ -1,21 +1,31 @@
-var checkingBalance = document.querySelector("div#checking_balance");
-checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+$(document).ready(function(){
+  var checking = new Account($("#checking"));
+  var savings = new Account($("#savings"));
+});
 
-var checkingDeposit = function(amount){
-  checkingBalance = parseInt( checkingBalance.innerHTML.replace("$", "") );
+function Account(element){
+  var account = this;
+  account.element = element;
+  account.balance = 0;
+  account.elements = {
+    deposit: account.element.find(".deposit"),
+    withdraw: account.element.find(".withdraw"),
+    balance: account.element.find(".balance"),
+    input: account.element.find(".input")
+  }
 
+  account.elements.deposit.click(function(){
+    var input = parseInt(account.elements.input.val());
+    account.balance += input;
+    account.elements.balance.text("$" + account.balance);
+  });
+
+  account.elements.withdraw.click(function(){
+    var input = parseInt(account.elements.input.val());
+    var newBalance = account.balance - input;
+    if(newBalance > 0){
+      account.balance = newBalance;
+      account.elements.balance.text("$" + account.balance);
+    }
+  });
 }
-
-function checkingWithdrawal(amount){
-
-}
-
-function savingsDeposit(amount){
-
-}
-
-function savingsWithdrawal(amount){
-
-}
-
-// an eventListerner for each button, each one a "click"
