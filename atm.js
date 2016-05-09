730d8205aab97aea2aaae23e881d9536814ca430
KAIRA N. JOHNSON <KJOHNSON1913@GMAIL.COM>
2016-05-09 19:35:40 -0400

    added transaction object and replace balance with input

--- atm.js
+++ atm.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-// Uh oh -- it's saying `$` is undefined! Something's missing from `index.html`...
+ var checking = new transaction($("#checking"));
+ console.log(checking);
+ var savings  = new transaction($("#savings"));
 });
+function transaction(entry){
+  var transaction = this;
+  transaction.entry = entry;
+  transaction.balance = 0;
+  transaction.entries = {
+    deposit: this.entry.find(".deposit"),
+    withdraw: this.entry.find(".withdraw"),
+    balance: this.entry.find(".balance"),
+    input: this.entry.find(".input")
+  }
+this.entries.deposit.on('click', function (){
+  var input = parseInt(transaction.entries.input.val());
+
+  transaction.entries.balance.replaceWith("<p>" + input + "</p>")
+ console.log(transaction.entries.input.val())
+})
+
+};
